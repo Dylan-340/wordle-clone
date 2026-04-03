579c13f9d648d0bafc35a2f0e10a7023c7f5df75
Dylan-340 <dylansb1126@gmail.com>
2026-04-03 11:28:08 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,34 @@
+# ⬛, 🟩, 🟨
+
+
 import random
+from os import system
+
 
 with open("wordle_words.txt", "r") as words_file:
     words = words_file.read().split("\n")
     words_file.close()
+
+system("clear")
+
+print("(ctrl+c to exit)\n\nWordle")
+
+
+while True:
+
+    game_boxes = ["⬛⬛⬛⬛⬛", "⬛⬛⬛⬛⬛", "⬛⬛⬛⬛⬛", "⬛⬛⬛⬛⬛", "⬛⬛⬛⬛⬛", "⬛⬛⬛⬛⬛"]
+    correct_word = random.choice(words)
+    for i in game_boxes:
+        print(i)
+    
+
+
+    guessed_word = input("Enter your first guess: ").lower()
+    
+    
+    
+
+    
+
+
+
